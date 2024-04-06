40cdf03f77fd3d7decf27e9fdfcf3d69ab48b5d1
Akos Oravecz <oraveczakos2001@gmail.com>
2024-04-06 13:54:05 +0200

    small fixes

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
-import { name as appName } from './app.json';
+import { expo } from './app.json';
 import App from './src/index';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+const appName = expo.name;
 
 export default function Main() {
   return (
